c130acbdc226b99c797c3a0e416e9fe2e7f5fcdc
Daimus Suudi <daimus.suudi@gmail.com>
2025-03-08 12:13:33 +0700

    refactor: change menu responsibility

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,8 +9,8 @@ import {cn} from "@/lib/utils";
 
 const menu = [
     {
-        title: "Home",
-        path: "/",
+        title: "Project",
+        path: "/project",
     },
     {
         title: "About",
@@ -26,13 +26,15 @@ export default function Navbar (){
         <>
             <header className="sticky top-0 backdrop-blur-xl py-2 z-50">
                 <nav className="px-4 md:px-0 py-2 max-w-2xl mx-auto flex justify-between items-center">
-
-                    <Link href="/" className=" hover:text-teal-500 hidden shrink-0 sm:block">
-                        <House />
-                    </Link>
-
-                    <div className="flex gap-4">
+                    <div className="w-full flex justify-between gap-4">
                         <ul className="flex items-center font-medium text-sm text-zinc-600 dark:text-zinc-400">
+                            <li>
+                                <Link href={'/'}
+                                      className={cn(`text-zinc-800 dark:text-zinc-300 hover:text-emerald-600 sm:px-4 sm:py-2 px-3 py-2 rounded-full bg-transparent`, pathname == '/' && `dark:text-gray-100 bg-zinc-200/50 dark:bg-zinc-800`)}
+                                >
+                                    <House className="inline h-5 w-5" />
+                                </Link>
+                            </li>
                             {menu.map((item, index) => (
                                 <li key={index}>
                                     <Link href={item.path}
@@ -43,15 +45,19 @@ export default function Navbar (){
                                 </li>
                             ))}
                         </ul>
-                        {
-                            theme === "dark" ? <Button onClick={() => setTheme("light")} variant="outline" size="icon" className="cursor-pointer">
-                                <Moon className="fill-white" />
-                            </Button> : <div>
-                                <Button onClick={() => setTheme("dark")} variant="outline" size="icon" className="cursor-pointer">
-                                    <Sun className="fill-black" />
-                                </Button>
-                            </div>
-                        }
+                        <div>
+                            {
+                                theme === "dark" ? <Button onClick={() => setTheme("light")} variant="outline" size="icon"
+                                                           className="cursor-pointer">
+                                    <Moon className="fill-white"/>
+                                </Button> : <div>
+                                    <Button onClick={() => setTheme("dark")} variant="outline" size="icon"
+                                            className="cursor-pointer">
+                                        <Sun className="fill-black"/>
+                                    </Button>
+                                </div>
+                            }
+                        </div>
                     </div>
                 </nav>
             </header>
