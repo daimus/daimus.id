ec249a39a3cbaca3907cebad3236a03a58a34cfe
Daimus Suudi <daimus.suudi@gmail.com>
2025-06-02 21:22:14 +0700

    fix: can not recognize qr in dark mode

--- src/components/buttons/donate/donate-qris-dialog.tsx
+++ src/components/buttons/donate/donate-qris-dialog.tsx
@@ -3,12 +3,12 @@ import {
     DialogContent,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import {QrCode} from "lucide-react";
-import {Button} from "@/components/ui/button";
+import { QrCode } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import QRCode from "react-qr-code";
-import {useSearchParams} from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
-export default function DonateQrisDialog (){
+export default function DonateQrisDialog() {
     const searchParams = useSearchParams();
 
     return (
@@ -16,16 +16,16 @@ export default function DonateQrisDialog (){
             <Dialog defaultOpen={searchParams.get("donate") === "qris"}>
                 <DialogTrigger className="w-full">
                     <Button variant="outline" className="w-full text-center cursor-pointer">
-                        <QrCode/>
+                        <QrCode />
                         QRIS
                     </Button>
                 </DialogTrigger>
                 <DialogContent>
-                    <div className="mt-8">
+                    <div className="mt-8 bg-white text-black">
                         <div className="p-4">
                             <QRCode
                                 size={256}
-                                style={{height: "auto", maxWidth: "100%", width: "100%"}}
+                                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
                                 value={process.env.NEXT_PUBLIC_QRIS_CODE || ""}
                                 viewBox={`0 0 256 256`}
                             />
