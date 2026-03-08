e0abf0b14a192c4dfd90352df90285a8179f06df
Daimus Suudi <daimus.suudi@gmail.com>
2026-03-08 07:52:57 +0700

    feat: update profile image and personal tagline on the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
     return (
         <>
             <div className="flex flex-col justify-center items-center w-full text-center mt-12">
-                <Image src="/images/daimus.jpg" alt="daimus suudi" width={200} height={200} className="rounded-full w-24 h-24" />
+                <Image src="/images/ava.jpeg" alt="daimus suudi" width={200} height={200} className="rounded-full w-24 h-24" />
                 <h1 className="font-semibold text-2xl">Daimus Suudi</h1>
                 <p className="text-base">
-                    Engineering Code, Cultivating Life
+                    Long life learner
                 </p>
             </div>
             <div className="w-full flex flex-col justify-center items-center space-y-4 mt-12">
